f361f3ce4a68043409ddae2d1093a34caba0cee8
Psaykho <jeremyandre.m@gmail.com>
2023-05-24 10:39:46 +0700

    fix: removed unused import

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_google_map_testing/popup_alert.dart';
